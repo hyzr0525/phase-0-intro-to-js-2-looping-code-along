5cb61770736834616ccb4d7c664600dd3cd87ee0
Yewei <yeweichen1@gmail.com>
2021-08-19 16:17:36 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+const eventName = "birthday";
+
+function writeCards(names, eventName){
+    let writtenLetters = [];
+    for (let n = 0; n < names.length; n++){
+       writtenLetters.push(`Thank you, ${names[n]}, for the wonderful ${eventName} gift!`);
+    }
+    return writtenLetters;
+    debugger;
+};
+
+function countDown(Number){
+    while (0 <= Number ){
+        console.log(Number --);
+    } 
+}
+
+countDown(10);
